f62f0627d030da9a5543ff6ee37179f873ed6cd4
Jonathan <jonathanfanamby@gmail.com>
2025-12-08 14:13:49 +0300

    encore

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import heart from "./assets/heart.svg"
 
 function Footer() {
   return (
     <div className='flex font-medium justify-center h-[56px] bg-[#040404] items-center text-[#c5c5c58c] text-[15px]'>
-        Make with <img src="./src/assets/heart.svg" style={{marginInline:"4px"}} className='w-[25px]' alt="" /> by Fanamby Jonathan
+        Make with <img src={heart} style={{marginInline:"4px"}} className='w-[25px]' alt="" /> by Fanamby Jonathan
     </div>
   )
 }
